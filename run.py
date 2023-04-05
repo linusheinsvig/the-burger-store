0eea64062fc2c94d23ffbbb8d11531d257e40154
Linus Heinsvig <heinsvig@icloud.com>
2023-04-05 09:11:21 +0000

    Add menu items and prices

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+burgers = {
+    "Original": 8,
+    "Cheeze": 9,
+    "Mexicana": 10,
+    "Paris": 10,
+    "Vegi": 9,
+}
+print(burgers)
+
+extras = {
+    "Bacon": 2,
+    "Cheese": 2,
+    "Extra Meat": 5,
+}
+print(extras)
+
+sides = {
+    "Fries": 3,
+    "Sweet Potato Fries": 3,
+    "Onion Rings": 4,
+    "Chili Cheese": 4,
+}
+print(sides)
+
+drinks = {
+    "Soda": 2,
+    "Water": 1,
+    "Milk": 2,
+    "Orange Juice": 3,
+}
+print(drinks)
